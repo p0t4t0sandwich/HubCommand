72a35ce7555a61fb49c21be949d3b0676435c41a
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2022-09-18 19:04:36 -0600

    first commit

---
+++ src/main/java/ca/sperrer/bungee/hubcommand/Hub.java
@@ -0,0 +1,22 @@
+package ca.sperrer.bungee.hubcommand;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.CommandSender;
+
+public class Hub extends Command {
+
+    public Hub() {
+        super("hub");
+    }
+    public void execute (CommandSender sender, String[]args){
+        if ((sender instanceof ProxiedPlayer)) {
+            ProxiedPlayer p = (ProxiedPlayer) sender;
+            p.sendMessage(new ComponentBuilder("Connecting you to the Hub!").color(ChatColor.AQUA).create());
+            p.connect(ProxyServer.getInstance().getServerInfo("Hub"));
+        }
+    }
+}
---
+++ src/main/java/ca/sperrer/bungee/hubcommand/HubCommand.java
@@ -0,0 +1,11 @@
+package ca.sperrer.bungee.hubcommand;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class HubCommand extends Plugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("has loaded!");
+        getProxy().getPluginManager().registerCommand(this, new Hub());
+    }
+}
